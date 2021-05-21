3ea962f36d0d90ead775dc9509b4c913bee6840d
AnthonyRodriguez93 <52090741+AnthonyRodriguez93@users.noreply.github.com>
2021-05-21 00:54:39 -0500

    fix

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -21,7 +21,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/signin', {
+        fetch('https://www.cryptic-beyond-08590.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
