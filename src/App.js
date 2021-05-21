faf02986b7fe95d155f2ef86b9a10d6436ff9b03
AnthonyRodriguez93 <52090741+AnthonyRodriguez93@users.noreply.github.com>
2021-05-21 00:58:23 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://www.cryptic-beyond-08590.herokuapp.com/imageurl', {
+    fetch('https://cryptic-beyond-08590.herokuapp.com/imageurl', {
           method: 'post',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
@@ -91,7 +91,7 @@ class App extends Component {
         .then(response => response.json())
         .then(response => {
           if(response){
-            fetch('https://www.cryptic-beyond-08590.herokuapp.com:3000/image', {
+            fetch('https://cryptic-beyond-08590.herokuapp.com:3000/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -21,7 +21,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://www.cryptic-beyond-08590.herokuapp.com/signin', {
+        fetch('https://cryptic-beyond-08590.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
